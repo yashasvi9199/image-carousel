a003545a850e7f6be228643e8297393f3bcab57c
Yash Haldiya <yashasvi9199@gmail.com>
2023-11-22 22:30:24 +0530

    added infinite loop in carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,20 +5,43 @@ import img2 from './images/img2.jpg'
 import img3 from './images/img3.jpg'
 
 function Carousel() {
+  let count=0
 
   let imageContainerRef = useRef(null)
 
   const images = [img1, img2, img3]
 
-  const prev = () => imageContainerRef.current.scrollLeft -= 250
-  const next = () => imageContainerRef.current.scrollLeft += 250
+  const prev = () => {
+
+    imageContainerRef.current.scrollLeft -= 250
+    --count
+    if(count < 0){
+      imageContainerRef.current.scrollLeft = 800
+      count = 2
+    }
+  }
+  const next = () => {
+
+    imageContainerRef.current.scrollLeft += 250
+    ++count
+    if(count > 2){
+      imageContainerRef.current.scrollLeft = 0
+      count = 0
+    }
+
+  }
 
   return (
     <div className="container">
         <div className="content">
             <div className="prev" onClick={prev}></div>
             <div className="slide-pannel" ref={imageContainerRef}>
-                {images.map(image => { return (<img src={image} />)})}
+                {images.map((imageArray,index) => { 
+                  return (
+                        <img key={index} src={imageArray} alt='landscape'/>
+                  )
+                  })
+                }
             </div>
             <div className="next" onClick={next}></div>
         </div>
